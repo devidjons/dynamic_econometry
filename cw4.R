b0412fa45ec53c6b7523db2266cc22f58953d8c4
devidjons <snozyk.v@gmail.com>
2019-03-28 11:44:40 +0100

    start test phi=1

--- cw4.R
+++ cw4.R
@@ -8,3 +8,7 @@ res=residuals(arima(x, c(1,0,0)))
 
 # Thu Mar 28 11:08:22 2019 ------------------------------
 library(CADFtest)
+w2=read.xlsx("Zaj4_AR.xlsx", 2, startRow = 4)
+
+# Thu Mar 28 11:37:52 2019 ------------------------------
+w3=read.xlsx("Zaj5_infl.xlsx", startRow = 3)
