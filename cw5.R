3c2e1b48b88b6b0f6b51ade18606231f5d35dc3c
Vlad <snozyk.v@gmail.com>
2019-04-04 09:59:56 +0200

    done homework

--- cw5.R
+++ cw5.R
@@ -1,1 +1,9 @@
 library(openxlsx)
+library(CADFtest)
+data=read.xlsx("Zaj5_infl.xlsx", startRow = 3)
+# Test DF dla danych o inflacji. One są w formie GUS wyrażonej jako GUS_inf=100*CPI(t)/CPI(t-12). Weźmy logarytmiczne stopy czyli 100*ln(GUS_inf/100). Test robimy w Excelu na danych od 2000 roku. Wypisujemy wnioski, patrzymy na założenia i staramy się oszacować ich poprawność.
+plot(data[data$r>2000,3])
+data$ret=log(lead(data$inflacja.GUS,12)/data$inflacja.GUS)
+data_2000=data%>%filter(r>=2000)
+plot(data_2000$ret)
+CADFtest(data_2000$ret)
